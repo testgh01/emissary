78d8fa398e698a8369e2502bbe53339e6454bf4b
flbruto <fbruton819@gmail.com>
2022-02-22 17:45:42 +0000

    Add parallel place startup feature

--- src/main/java/emissary/admin/Startup.java
+++ src/main/java/emissary/admin/Startup.java
@@ -9,10 +9,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import emissary.config.Configurator;
 import emissary.config.ServiceConfigGuide;
 import emissary.core.EmissaryException;
+import emissary.core.Namespace;
 import emissary.directory.EmissaryNode;
 import emissary.directory.KeyManipulator;
 import emissary.pickup.PickUpPlace;
@@ -25,6 +28,7 @@ public class Startup {
     public static final int DIRECTORYSTART = 0;
     public static final int DIRECTORYADD = 1;
     public static final int DIRECTORYDELETE = 2;
+    private static final String PARALELL_PLACE_STARTUP_CONFIG = "PARALELL_PLACE_STARTUP";
     static int directoryAction = DIRECTORYADD;
 
     // If we are an emissary node these will be present
@@ -321,69 +325,75 @@ public class Startup {
             @Override
             public void run() {
 
-                final String thePlaceHost = placeHost(hostParameters.get(0));
-
-                final String localDirectory = localDirectoriesArg.get(thePlaceHost);
-
-                if (localDirectory == null) {
-                    Startup.this.placesToStart.removeAll(hostParameters);
-                    if (Startup.this.failedLocalDirectories.get(thePlaceHost) != null) {
-                        logger.warn("Skipping {} due to previously failed directory", thePlaceHost);
-                    } else {
-                        logger.warn("Skipping {} : local Directory not found", thePlaceHost);
-                    }
-                    return;
-                }
-
-                if (directoryActionArg != DIRECTORYSTART && directoryActionArg != DIRECTORYADD) {
-                    Startup.this.placesToStart.removeAll(hostParameters);
-                    return;
-                }
-
-                logger.info("Using localDir={} to create {} places on {}", localDirectory, hostParameters.size(), thePlaceHost);
-
-                // Start everything in hostParameters
-                // (PLACE lines from cfg file for a given host
-                for (int i = 0; i < hostParameters.size(); i++) {
-                    final String thePlaceLocation = hostParameters.get(i);
-                    placeName(thePlaceLocation);
-
-                    // Get the class name and Class object for what we want to make
-                    final String thePlaceClassString = PlaceStarter.getClassString(thePlaceLocation);
-                    if (thePlaceClassString == null) {
-                        logger.warn("Skipping {}, no class string", thePlaceLocation);
-                        Startup.this.placesToStart.remove(thePlaceLocation);
-                        continue;
-                    }
-
-                    logger.debug("Starting place {}", thePlaceLocation);
-                    if (KeyManipulator.isLocalTo(thePlaceLocation,
-                            String.format("http://{}:{}/StartupEngine", Startup.this.node.getNodeName(), Startup.this.node.getNodePort()))) {
-                        if (directoryActionArg == DIRECTORYADD && emissary.core.Namespace.exists(thePlaceLocation)) {
-                            logger.info("Local place already exists: {}", thePlaceLocation);
-                            Startup.this.placesToStart.remove(thePlaceLocation);
-                            continue;
-                        }
-
-                        logger.info("Doing local startup on place {}", thePlaceLocation);
-                        final String thePlaceClassStr = PlaceStarter.getClassString(thePlaceLocation);
-                        final IServiceProviderPlace p =
-                                PlaceStarter.createPlace(thePlaceLocation, (InputStream) null, thePlaceClassStr, localDirectory);
-                        if (p != null) {
-                            placesArg.put(thePlaceLocation, thePlaceLocation);
-                        } else {
-                            Startup.this.placesToStart.remove(thePlaceLocation);
-                            logger.debug("Giving up on {}", thePlaceLocation);
-                        }
-
-                        continue;
-                    }
-                }
+                runPlaceSetup(directoryActionArg, localDirectoriesArg, placesArg, hostParameters);
             }
         });
 
         t.start();
         return true;
+    }
+
+    protected void runPlaceSetup(int directoryActionArg, Map<String, String> localDirectoriesArg, Map<String, String> placesArg,
+            List<String> hostParameters) {
+        final String thePlaceHost = placeHost(hostParameters.get(0));
+
+        final String localDirectory = localDirectoriesArg.get(thePlaceHost);
+
+        if (localDirectory == null) {
+            Startup.this.placesToStart.removeAll(hostParameters);
+            if (Startup.this.failedLocalDirectories.get(thePlaceHost) != null) {
+                logger.warn("Skipping {} due to previously failed directory", thePlaceHost);
+            } else {
+                logger.warn("Skipping {} : local Directory not found", thePlaceHost);
+            }
+            return;
+        }
+
+        if (directoryActionArg != DIRECTORYSTART && directoryActionArg != DIRECTORYADD) {
+            Startup.this.placesToStart.removeAll(hostParameters);
+            return;
+        }
+
+        logger.info("Using localDir={} to create {} places on {}", localDirectory, hostParameters.size(), thePlaceHost);
+
+        // Start everything in hostParameters
+        // (PLACE lines from cfg file for a given host
+        // Get the class name and Class object for what we want to make
+
+        boolean paralellPlaceStartup = hostsConfig.findBooleanEntry(PARALELL_PLACE_STARTUP_CONFIG, false);
+        Stream<String> hostParametersStream = StreamSupport.stream(hostParameters.spliterator(), paralellPlaceStartup);
+        logger.info("Parallel startup place flag {}", hostParametersStream.isParallel());
+
+        hostParametersStream.forEach(thePlaceLocation -> {
+            placeName(thePlaceLocation);
+            final String thePlaceClassString = PlaceStarter.getClassString(thePlaceLocation);
+            if (thePlaceClassString == null) {
+                logger.warn("Skipping {}, no class string", thePlaceLocation);
+                Startup.this.placesToStart.remove(thePlaceLocation);
+                return;
+            }
+            logger.debug("Starting place {}", thePlaceLocation);
+            if (KeyManipulator.isLocalTo(thePlaceLocation,
+                    String.format("http://%s:%s/StartupEngine", Startup.this.node.getNodeName(), Startup.this.node.getNodePort()))) {
+                if (directoryActionArg == DIRECTORYADD && Namespace.exists(thePlaceLocation)) {
+                    logger.info("Local place already exists: {}", thePlaceLocation);
+                    Startup.this.placesToStart.remove(thePlaceLocation);
+                    return;
+                }
+
+                logger.info("Doing local startup on place {}", thePlaceLocation);
+                final String thePlaceClassStr = PlaceStarter.getClassString(thePlaceLocation);
+                final IServiceProviderPlace p =
+                        PlaceStarter.createPlace(thePlaceLocation, null, thePlaceClassStr, localDirectory);
+                if (p != null) {
+                    placesArg.put(thePlaceLocation, thePlaceLocation);
+                } else {
+                    Startup.this.placesToStart.remove(thePlaceLocation);
+                    logger.debug("Giving up on {}", thePlaceLocation);
+                }
+
+            }
+        });
     }
 
     /**
--- src/test/java/emissary/admin/StartupTest.java
+++ src/test/java/emissary/admin/StartupTest.java
@@ -6,7 +6,10 @@ import static org.hamcrest.Matchers.containsInAnyOrder;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import emissary.config.Configurator;
+import emissary.directory.DirectoryPlace;
 import emissary.directory.EmissaryNode;
 import emissary.pickup.file.FilePickUpClient;
 import emissary.pickup.file.FilePickUpPlace;
@@ -14,6 +17,7 @@ import emissary.place.CoordinationPlace;
 import emissary.place.sample.DelayPlace;
 import emissary.place.sample.DevNullPlace;
 import emissary.test.core.UnitTest;
+import org.apache.commons.collections4.map.HashedMap;
 import org.junit.Test;
 
 public class StartupTest extends UnitTest {
@@ -22,12 +26,7 @@ public class StartupTest extends UnitTest {
     public void testSortPlaces() throws IOException {
         List<String> somePlaces = new ArrayList<>();
         EmissaryNode node = new EmissaryNode();
-        String location = "http://" + node.getNodeName() + ":" + node.getNodePort();
-        somePlaces.add(location + "/" + CoordinationPlace.class.getSimpleName());
-        somePlaces.add(location + "/" + DelayPlace.class.getSimpleName());
-        somePlaces.add(location + "/" + DevNullPlace.class.getSimpleName());
-        somePlaces.add(location + "/" + FilePickUpPlace.class.getSimpleName());
-        somePlaces.add(location + "/" + FilePickUpClient.class.getSimpleName());
+        String location = createPlaceList(node, somePlaces);
 
 
         Startup startup = new Startup(node.getNodeConfigurator(), node);
@@ -41,4 +40,36 @@ public class StartupTest extends UnitTest {
         assertThat(places, containsInAnyOrder(location + "/" + "CoordinationPlace", location + "/" + "DelayPlace", location + "/" + "DevNullPlace"));
     }
 
+    @Test
+    public void testParallelStartupPlaceSetup() throws IOException {
+        List<String> somePlaces = new ArrayList<>();
+        EmissaryNode node = new EmissaryNode();
+        String location = createPlaceList(node, somePlaces);
+
+        Configurator nodeConfigurator = node.getNodeConfigurator();
+        nodeConfigurator.removeEntry("PARALELL_PLACE_STARTUP", "*");
+        nodeConfigurator.addEntry("PARALELL_PLACE_STARTUP", "true");
+
+
+        Startup startup = new Startup(nodeConfigurator, node);
+        startup.placesToStart.addAll(somePlaces);
+
+        Map<String, String> localDirectories = new HashedMap<>();
+        localDirectories.put("", location + "/" + DirectoryPlace.class.getSimpleName());
+        Map<String, String> places = new HashedMap<>();
+        startup.runPlaceSetup(Startup.DIRECTORYADD, localDirectories, places, somePlaces);
+
+        assertThat(places.values(), containsInAnyOrder(location + "/" + "CoordinationPlace", location + "/" + "DelayPlace",
+                location + "/" + "DevNullPlace", location + "/" + "FilePickUpPlace", location + "/" + "FilePickUpClient"));
+    }
+
+    private String createPlaceList(EmissaryNode node, List<String> somePlaces) {
+        String location = "http://" + node.getNodeName() + ":" + node.getNodePort();
+        somePlaces.add(location + "/" + CoordinationPlace.class.getSimpleName());
+        somePlaces.add(location + "/" + DelayPlace.class.getSimpleName());
+        somePlaces.add(location + "/" + DevNullPlace.class.getSimpleName());
+        somePlaces.add(location + "/" + FilePickUpPlace.class.getSimpleName());
+        somePlaces.add(location + "/" + FilePickUpClient.class.getSimpleName());
+        return location;
+    }
 }
